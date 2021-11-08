5e85eb2a876e59299f3a38119869778169e7f5d8
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2021-11-08 12:13:25 +0530

    User Input and parsing

--- demo-03.js
+++ demo-03.js
@@ -1,9 +1,11 @@
 // Demo-03.js
 // Operators : Arithmetic, Conditional , Logical
 // Control Statements: Conditional and Iterations
-let amt = 1000; 
-let rt  = 5.5;
-let dur = 36;
+
+// prompt for User input (string) and parseFloat to convert into number
+let amt = parseFloat(prompt('Enter principle amount: ' ,10000));
+let rt  = parseFloat(prompt('Enter rate of interest: ',5.5));
+let dur = parseInt(prompt('Enter duration in months: ',36));
 
 if (amt < 10000 || amt > 10000000) {
     alert("Amount is outside the range of 10,000 <-> 1,00,00,00,000");
